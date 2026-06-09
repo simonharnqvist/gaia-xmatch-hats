789290471bb6ad7a62c2b8690364fd6171f1a831
simonharnqvist <seharnqvist@gmail.com>
2026-06-09 08:09:50 +0000

    ztf wired in

--- src/gaia_xmatch_hats/assets.py
+++ src/gaia_xmatch_hats/assets.py
@@ -12,10 +12,11 @@ def gaia_hats(context):
         file_reader=ParquetPyarrowReader(chunksize=100_000),
         use_schema_file=paths.gaia_schema,
         output_artifact_name="gaia_hats",
-        output_path=paths.output_hats_dir,
+        output_path=paths.gaia_hats_dir,
     )
 
     pipeline_with_client(gaia_args, context.resources.dask_client)
+
 
 @asset(required_resource_keys={"dask_client", "paths"}, deps=["gaia_hats"])
 def gaia_ztf_xmatched(context):
--- src/gaia_xmatch_hats/definitions.py
+++ src/gaia_xmatch_hats/definitions.py
@@ -2,8 +2,7 @@ from gaia_xmatch_hats.assets import gaia_hats, gaia_ztf_xmatched
 from gaia_xmatch_hats.resources import DaskClientResource, CatalogPathsResource
 
 defs = Definitions(
-    assets=[gaia_hats, gaia_ztf_xmatched
-    ],
+    assets=[gaia_hats, gaia_ztf_xmatched],
     resources={
         "dask_client": DaskClientResource.configure_at_launch(),
         "paths": CatalogPathsResource.configure_at_launch()
