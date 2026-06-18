28685aa97270655b190475fa219bcaa3482dcd45
simonharnqvist <seharnqvist@gmail.com>
2026-06-18 09:32:14 +0000

    PS1 draft

--- src/gaia_xmatch_hats/assets.py
+++ src/gaia_xmatch_hats/assets.py
@@ -6,22 +6,6 @@ from pathlib import Path
 import subprocess
 
 
-@asset(required_resource_keys={"paths"})
-def ztf_hats(context):
-
-    ztf_hats_dir = Path(context.resources.paths.ztf_hats_dir).resolve()
-    ztf_hats_dir.mkdir(parents=True, exist_ok=True)
-
-    cmd = [
-        "aws", "s3", "cp",
-        "--recursive",
-        "--no-sign-request",
-        "s3://ipac-irsa-ztf/ztf/enhanced/dr24/lc/hats/",
-        str(ztf_hats_dir),
-    ]
-
-    subprocess.run(cmd, check=True)
-
 @asset(required_resource_keys={"dask_client", "paths"}, deps="ztf_hats")
 def gaia_hats(context):
     paths = context.resources.paths
@@ -46,6 +30,23 @@ def gaia_hats(context):
     pipeline_with_client(gaia_args, client)
 
 
+@asset(required_resource_keys={"paths"})
+def ztf_hats(context):
+
+    ztf_hats_dir = Path(context.resources.paths.ztf_hats_dir).resolve()
+    ztf_hats_dir.mkdir(parents=True, exist_ok=True)
+
+    cmd = [
+        "aws", "s3", "cp",
+        "--recursive",
+        "--no-sign-request",
+        "s3://ipac-irsa-ztf/ztf/enhanced/dr24/lc/hats/",
+        str(ztf_hats_dir),
+    ]
+
+    subprocess.run(cmd, check=True)
+
+
 @asset(required_resource_keys={"dask_client", "paths"}, deps=["gaia_hats"])
 def gaia_ztf_xmatched(context):
 
@@ -53,12 +54,71 @@ def gaia_ztf_xmatched(context):
 
     gaia_hats_path = Path(paths.gaia_hats_dir).resolve().joinpath(paths.gaia_hats_artifact_name)
     ztf_hats_path = Path(paths.ztf_hats_dir).resolve().joinpath(paths.ztf_hats_artifact_name)
-    xmatched_hats_dir = Path(paths.xmatched_hats_dir).resolve()
+    xmatched_hats_dir = Path(paths.gaia_ztf_xmatched_dir).resolve()
 
     client = context.resources.dask_client.create_or_get_client()
 
     crossmatch_with_gaia(gaia_hats = gaia_hats_path,
                         other_hats = ztf_hats_path,
                         other_catalog_name = "ztf",
+                        xmatch_dir = xmatched_hats_dir,
+                        dask_client = client)
+
+
+@asset(required_resource_keys={"dask_client", "paths"}, deps=["gaia_hats"])
+def ps1_hats(context):
+    paths = context.resources.paths
+
+    client = context.resources.dask_client.create_or_get_client()
+
+    PS1_PATH = UPath(path.ps1_hats_url, anon=True)
+    PS1_OBJECT = PS1_PATH / "otmo"
+    PS1_DETECTION = PS1_PATH / "detection"
+
+    ps1_object = open_catalog(
+        PS1_OBJECT,
+        columns=[
+            "objID",
+            "raMean",
+            "decMean",
+            "nStackDetections"
+        ])
+
+    ps1_detection = open_catalog(
+        PS1_DETECTION,
+        columns=[
+            "objID",
+            "detectID",
+            "obsTime",
+            "filterID",
+            "psfFlux",
+            "psfFluxErr",
+            "airMass", 
+            "sky", 
+            "skyErr", 
+            "apFlux", 
+            "apFluxErr", 
+            "zp"
+        ]
+    )
+
+    ps1_object.write_catalog(base_catalog_path=paths.ps1_hats_dir, catalog_name="PS1_object")
+    ps1_detection.write_catalog(base_catalog_path=paths.ps1_hats_dir, catalog_name="PS1_detection")
+
+
+@asset(required_resource_keys={"dask_client", "paths"}, deps=["gaia_hats"])
+def gaia_ps1_xmatched(context):
+
+    paths = context.resources.paths
+
+    gaia_hats_path = Path(paths.gaia_hats_dir).resolve().joinpath(paths.gaia_hats_artifact_name)
+    ps1_hats_path = Path(paths.ps1_hats_dir).resolve().joinpath("PS1_detection")
+    xmatched_hats_dir = Path(paths.gaia_ps1_xmatched_dir).resolve()
+
+    client = context.resources.dask_client.create_or_get_client()
+
+    crossmatch_with_gaia(gaia_hats = gaia_hats_path,
+                        other_hats = ps1_hats_path,
+                        other_catalog_name = "ps1",
                         xmatch_dir = xmatched_hats_dir,
                         dask_client = client)
